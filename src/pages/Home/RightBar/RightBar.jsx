2e1a0d7688409c0307cbaa1728baf7bdc23ce961
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-12-07 16:09:12 +0600

    search btn

--- src/pages/Home/RightBar/RightBar.jsx
+++ src/pages/Home/RightBar/RightBar.jsx
@@ -1,6 +1,43 @@
+import { useRef, useState } from "react";
+
 const RightBar = () => {
+  const [search, setSearch] = useState("");
+
+  const searchRef = useRef(null);
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
+  console.log(search);
   return (
-    <div className="mr-16">
+    <div className="mr-16 ml-4">
+      {/* search bar */}
+      <div className="form-control">
+        <div className="input-group flex justify-between items-center">
+          <input
+            type="text"
+            placeholder="Search…"
+            ref={searchRef}
+            className="input input-bordered rounded-md border-gray-300 focus:outline-[#0B0016] bg-gray-200 text-gray-900 p-2"
+          />
+          <button onClick={handleSearch} className="btn btn-square">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
       {/* suggestion user to follow */}
       <div className="mt-8">
         <div className="flex justify-between items-center">
