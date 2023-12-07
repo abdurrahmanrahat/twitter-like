fecb797aa8d38e7411d6e818fe6f01f2379c8eb6
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-12-07 14:06:49 +0600

    firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeT4-bgvr-2aq6dJD-O4CwjiU2M0imo40",
+  authDomain: "twitter-lite-47.firebaseapp.com",
+  projectId: "twitter-lite-47",
+  storageBucket: "twitter-lite-47.appspot.com",
+  messagingSenderId: "593498161319",
+  appId: "1:593498161319:web:1503966945f6c837801b82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
